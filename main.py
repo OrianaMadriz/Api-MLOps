a1a5512e87398f9f515831baee5b63faad813319
OrianaMadriz <orianamadriz29@gmail.com>
2023-05-16 12:22:41 -0300

    last commit

--- main.py
+++ main.py
@@ -9,50 +9,65 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = FastAPI()
 
 # Cargamos el dataset para las funciones
-data = pd.read_csv('Datasets\data_funciones.csv')
+data = pd.read_csv('Datasets/data_funciones.csv')
 
 @app.get("/peliculas_mes/{mes}")
 def peliculas_mes(mes: str):
     # Nos quedamos solo con un dataframe con las peliculas de ese mes
     df_mes = data[data['mes'].str.lower() == mes.lower()]
     
-    # Obtenemos la cantidad de películas estrenadas ese mes
-    cantidad_peliculas = len(df_mes)
-    
-    # Devolvemos un diccionario con el nombre del mes y la cantidad de películas estrenadas ese mes
-    return {'Mes': mes.capitalize(), 'Cantidad': cantidad_peliculas}
+    #Si la productora ingresada no esta en las productora del dataset devolvemos el mensaje que no se tiene datos del valor ingresado
+    if mes not in df_mes.values:
+        return {'mensaje': "No se tienen datos del mes ingresado" }
+    else:
+
+        # Obtenemos la cantidad de películas estrenadas ese mes
+        cantidad_peliculas = len(df_mes)
+        
+        # Devolvemos un diccionario con el nombre del mes y la cantidad de películas estrenadas ese mes
+        return {'Mes': mes.capitalize(), 'Cantidad': cantidad_peliculas}
 
 @app.get("/peliculas_dia/{dia}")
 def peliculas_dia(dia: str):
     # Nos quedamos solo con un dataframe con las peliculas de ese dia
     df_dia = data[data['dia'].str.lower() == dia.lower()]
     
-    # Obtenemos la cantidad de películas estrenadas ese dia
-    cantidad_peliculas = len(df_dia)
-    
-    # Devolvemos un diccionario con el nombre del dia y la cantidad de películas estrenadas ese dia
-    return {'Dia': dia.capitalize(), 'Cantidad': cantidad_peliculas}
+    #Si la productora ingresada no esta en las productora del dataset devolvemos el mensaje que no se tiene datos del valor ingresado
+    if dia not in df_dia.values:
+        return {'mensaje': "No se tienen datos del dia ingresado" }
+    else:
+
+        # Obtenemos la cantidad de películas estrenadas ese dia
+        cantidad_peliculas = len(df_dia)
+        
+        # Devolvemos un diccionario con el nombre del dia y la cantidad de películas estrenadas ese dia
+        return {'Dia': dia.capitalize(), 'Cantidad': cantidad_peliculas}
 
 @app.get("/franquicia/{franquicia}")
 def franquicia(franquicia: str):
       # Nos quedamos solo con un dataframe con las peliculas de esa colección proporcionada
     df_franquicia = data[data['collection_name'] == franquicia]
 
-    # Obtenemos la cantidad de películas estrenadas ese dia
-    cantidad_peliculas = len(df_franquicia)
-    
-    # Calculamos el promedio y el total de la ganancia para las películas de la colección
-    ganancia_promedio = df_franquicia['revenue'].mean()
-    ganancia_total = df_franquicia['revenue'].sum()
-    
-    # Devolvemos un diccionario con la información
-    return {
-            'Franquicia': franquicia,
-            'Cantidad':cantidad_peliculas,
-            'Ganancia_total': ganancia_total,
-            'Ganancia_promedio': ganancia_promedio
+    #Si la productora ingresada no esta en las productora del dataset devolvemos el mensaje que no se tiene datos del valor ingresado
+    if franquicia not in df_franquicia.values:
+        return {'mensaje': "No se tienen datos de la franquicia ingresada" }
+    else:
+
+        # Obtenemos la cantidad de películas estrenadas ese dia
+        cantidad_peliculas = len(df_franquicia)
         
-    }
+        # Calculamos el promedio y el total de la ganancia para las películas de la colección
+        ganancia_promedio = df_franquicia['revenue'].mean()
+        ganancia_total = df_franquicia['revenue'].sum()
+        
+        # Devolvemos un diccionario con la información
+        return {
+                'Franquicia': franquicia,
+                'Cantidad':cantidad_peliculas,
+                'Ganancia_total': ganancia_total,
+                'Ganancia_promedio': ganancia_promedio
+            
+        }
 
 @app.get("/peliculas_pais/{pais}")
 def peliculas_pais(pais: str):
@@ -60,14 +75,19 @@ def peliculas_pais(pais: str):
     columnas_pais = [f'pco_name_{i}' for i in range(25)]
     df_pais = data[data[columnas_pais].eq(pais).any(axis=1)]    
     
-    # Obtenemos la cantidad de películas producidas en el país
-    cantidad = df_pais.shape[0]
-    
-    # Devolvemos un diccionario con la información
-    return {
-        'Pais': pais,
-        'Cantidad': cantidad
-    }
+    #Si la productora ingresada no esta en las productora del dataset devolvemos el mensaje que no se tiene datos del valor ingresado
+    if pais not in df_pais.values:
+        return {'mensaje': "No se tienen datos del pais ingresado" }
+    else:
+
+        # Obtenemos la cantidad de películas producidas en el país
+        cantidad = df_pais.shape[0]
+        
+        # Devolvemos un diccionario con la información
+        return {
+            'Pais': pais,
+            'Cantidad': cantidad
+        }
 
 @app.get("/productoras/{productora}")
 def productoras(productora: str):
@@ -75,18 +95,23 @@ def productoras(productora: str):
     columnas_productoras = [f'pc_name_{i}' for i in range(26)]
     df_pc = data[data[columnas_productoras].eq(productora).any(axis=1)]
 
-    # Calculamos la ganancia total de las películas de la productora
-    ganancia_total = df_pc['revenue'].sum()
+    #Si la productora ingresada no esta en las productora del dataset devolvemos el mensaje que no se tiene datos del valor ingresado
+    if productora not in df_pc.values:
+        return {'mensaje': "No se tienen datos de la productora ingresada" }
+    else:
 
-    # Contamos la cantidad de películas asociadas a la productora
-    cantidad = df_pc.shape[0]
+        # Calculamos la ganancia total de las películas de la productora
+        ganancia_total = df_pc['revenue'].sum()
 
-    # Devolvemos un diccionario con la información
-    return {
-        'Productora': productora,
-        'Ganancia_total': ganancia_total,
-        'Cantidad': cantidad
-    }
+        # Contamos la cantidad de películas asociadas a la productora
+        cantidad = df_pc.shape[0]
+
+        # Devolvemos un diccionario con la información
+        return {
+            'Productora': productora,
+            'Ganancia_total': ganancia_total,
+            'Cantidad': cantidad
+        }
 
 # Convertimos columnas numéricas a tipos de datos apropiados
 data['budget'] = pd.to_numeric(data['budget'], errors='coerce')
@@ -97,35 +122,40 @@ data['release_year'] = pd.to_numeric(data['release_year'], errors='coerce')
 
 @app.get("/retorno/{pelicula}")
 def retorno(pelicula: str):
-   # Eliminamos filas duplicadas basadas en la columna 'title'
-    data_unica = data.drop_duplicates(subset='title')
+   #Si la pelicula ingresada no esta en los titulos del dataset devolvemos el mensaje que no se tiene datos del valor ingresado
+    if pelicula not in data['title'].values:
+        return {'mensaje': "No se tienen datos del titulo ingresado" }
+    else:
+   
+    # Eliminamos filas duplicadas basadas en la columna 'title'
+        data_unica = data.drop_duplicates(subset='title')
 
-    # Nos quedamos solo con un dataframe con los datos de esa película proporcionada
-    data_pelicula = data_unica[data_unica['title'] == pelicula]
+        # Nos quedamos solo con un dataframe con los datos de esa película proporcionada
+        data_pelicula = data_unica[data_unica['title'] == pelicula]
 
-    # Verificamos si se encontró la película
-    if len(data_pelicula) == 0:
-        return {'error': 'Película no encontrada'}
+        # Verificamos si se encontró la película
+        if len(data_pelicula) == 0:
+            return {'error': 'Película no encontrada'}
 
-    # Obtenemos los valores de las columnas de interés
-    inversion = data_pelicula['budget'].iloc[0].item()
-    ganancia = data_pelicula['revenue'].iloc[0].item()
-    retorno = data_pelicula['return'].iloc[0].item()
-    anio = data_pelicula['release_year'].iloc[0].item()
+        # Obtenemos los valores de las columnas de interés
+        inversion = data_pelicula['budget'].iloc[0].item()
+        ganancia = data_pelicula['revenue'].iloc[0].item()
+        retorno = data_pelicula['return'].iloc[0].item()
+        anio = data_pelicula['release_year'].iloc[0].item()
 
-    # Devolvemos un diccionario con la información
-    informacion_pelicula = {
-        'Pelicula': pelicula,
-        'Inversion': inversion,
-        'Ganancia': ganancia,
-        'Retorno': retorno,
-        'Anio': anio
-    }
+        # Devolvemos un diccionario con la información
+        informacion_pelicula = {
+            'Pelicula': pelicula,
+            'Inversion': inversion,
+            'Ganancia': ganancia,
+            'Retorno': retorno,
+            'Anio': anio
+        }
 
-    return informacion_pelicula
+        return informacion_pelicula
 
 # Cargamos el dataset para aplicar el modelo
-df = pd.read_csv('Datasets\data_modelo.csv')
+df = pd.read_csv('Datasets/data_modelo.csv')
 
 @app.get("/recomendacion/{titulo}")
 def recomendacion(titulo: str):
